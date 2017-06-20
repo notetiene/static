f23aa73a72998e11b8e4a125b86bd3692e138130
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2017-06-20 16:34:15 -0400

    Update lein dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject static "1.1.0-SNAPSHOT"
   :description "Simple static site generator."
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.2"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [cssgen "0.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [hiccup "1.0.5"]
                  [hiccup-bridge "1.0.1"]
                  [enlive "1.1.6"]
-                 [org.pegdown/pegdown "1.5.0"]
+                 [org.pegdown/pegdown "1.6.0"]
                  [org.clojars.amit/commons-io "1.4.0"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-core "1.6.1"]
+                 [ring/ring-jetty-adapter "1.6.1"]
                  [watchtower "0.1.1"]
                  [stringtemplate-clj "0.1.0"]]
   :main static.core
